bb0e45179f3320394c2637bbc5dacf169cd33cfb
“shubham71099” <shubham71099@gmail.com>
2026-01-17 11:20:11 +0530

    Backend url changed

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://url-shortner-h0vd.onrender.com",
 });
 
 export const createShortUrl = (url) =>
